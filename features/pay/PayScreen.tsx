3383d8c23dbd6daef20c0497e84ea7a280c25847
ll220l0 <72867889+ll220l0@users.noreply.github.com>
2026-03-30 18:14:48 +0600

    Import next/image in pay screen

--- features/pay/PayScreen.tsx
+++ features/pay/PayScreen.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useRef, useState } from "react";
 import toast from "react-hot-toast";
